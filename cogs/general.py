f19b27180a5360c78fdc1087510ce9f535e72d46
peyrin <peyrin@berkeley.edu>
2017-09-18 01:41:58 -0700

    Split general and goodchat-specific commands

--- cogs/general.py
+++ cogs/general.py
@@ -178,56 +178,6 @@ class General:
         await self.bot.say(msg)
 
     @commands.command(pass_context=True, no_pm=True)
-    async def whodunit(self, ctx):
-        """Whodunit?"""
-        server = ctx.message.server
-        member_list = list(server.members)
-        await self.bot.say("It was " + choice(member_list).display_name + "!!!")
-
-    @commands.command(name="fahrenheit")
-    async def fahrenheit(self, *, temp : int):
-        """Converts Fahrenheit temperature to Celsius
-        """
-        newtemp = (temp-32)*5.0/9.0
-        if newtemp < 0:
-            await self.bot.say(str(temp) + " degrees Fahrenheit is " + str(newtemp)[:5] + " degrees Celsius.")
-        else:
-            await self.bot.say(str(temp) + " degrees Fahrenheit is " + str(newtemp)[:4] + " degrees Celsius.")
-
-    @commands.command(name="kelvin")
-    async def kelvin(self, *, temp : int):
-        """Converts Kelvin temperatures...for some reason
-        """
-        newcelsius = temp - 273.15
-        newfahrenheit = ((temp - 273.15) * 9.0/5.0) + 32
-        await self.bot.say(str(temp) + " Kelvin is " + str(round(newcelsius, 2)) + " degrees Celsius and " + str(round(newfahrenheit, 2)) + " degrees Fahrenheit.")
-
-    @commands.command(name="celsius")
-    async def celsius(self, *, temp : int):
-        """Converts Celsius temperature to Fahrenheit
-        """
-        newtemp = (temp * 9.0/5.0) + 32
-        await self.bot.say(str(temp) + " degrees Celsius is " + str(newtemp) + " degrees Fahrenheit.")
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def sue(self, ctx, user : discord.Member):
-        """Take a user to court."""
-        if user.id == self.bot.user.id:
-                user = ctx.message.author
-                await self.bot.say("Nice try. You think this is funny? How about let's sue {}, huh?".format(user.mention))
-        else:
-            await self.bot.say("{} has been sued".format(user.mention))
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def slap(self, ctx, user : discord.Member):
-        """Slaps a user."""
-        if user.id == self.bot.user.id:
-                user = ctx.message.author
-                await self.bot.say("Nice try. You think this is funny? *POW!* *slaps {}*".format(user.mention))
-        else:
-            await self.bot.say("*POW!* {} has been slapped".format(user.mention))
-
-    @commands.command(pass_context=True, no_pm=True)
     async def userinfo(self, ctx, *, user: discord.Member=None):
         """Shows users's informations"""
         author = ctx.message.author
