d37be206319c8dbbd5582af4b0ee696295294c67
peyrin <peyrin@berkeley.edu>
2018-03-31 14:51:57 -0700

    typo 3

--- red.py
+++ red.py
@@ -344,7 +344,8 @@ def initialize(bot_class=Bot, formatter_class=Formatter):
     async def on_message(message):
         channel = bot.get_channel('361948610994241537')
         if random.randint(0,1) == 0:
-            await bot.add_reaction(message, "<:catstare:429757543816888331>")
+            emoji = discord.utils.get(bot.get_all_emojis(), id='429757543816888331')
+            await bot.add_reaction(message, emoji)
         bot.counter["messages_read"] += 1
         if bot.user_allowed(message):
             await bot.process_commands(message)
